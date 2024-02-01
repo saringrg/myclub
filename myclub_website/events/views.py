459b1e44e8ae348ad058f81db6cf9a11a3fd9263
saringrg <saringrg168@gmail.com>
2024-02-01 10:06:02 +0545

    version 1.0

---
+++ myclub_website/events/views.py
@@ -0,0 +1,143 @@
+from django.shortcuts import render, redirect
+import calendar
+from calendar import HTMLCalendar 
+from datetime import datetime
+from django.http import HttpResponseRedirect
+from .models import Event, Venue
+from .forms import VenueForm, EventForm
+
+#import pagination stuff
+from django.core.paginator import Paginator
+
+
+
+#delete an venue
+def delete_venue(request, venue_id):
+	venue = Venue.objects.get(pk=venue_id)
+	venue.delete()
+	return redirect('list-venues')
+
+#delete an event
+def delete_event(request, event_id):
+	event = Event.objects.get(pk=event_id)
+	event.delete()
+	return redirect('list-events')
+
+
+def update_event(request, event_id):
+	event = Event.objects.get(pk=event_id)
+	form = EventForm(request.POST or None, instance=event)
+	if form.is_valid():
+		form.save()
+		return redirect('list-events')
+
+	return render(request, 'events/update_event.html', 
+		{'event': event, 'form':form}) 
+
+def add_event(request):
+	submitted = False
+	if request.method == "POST":
+		form = EventForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect('/add_event?submitted=True')
+	else:
+		form = EventForm
+		if 'submitted' in request.GET:
+			submitted = True
+			
+	return render(request, 'events/add_event.html', {'form':form, 'submitted':submitted}) 
+
+
+def update_venue(request, venue_id):
+	venue = Venue.objects.get(pk=venue_id)
+	form = VenueForm(request.POST or None, instance=venue)
+	if form.is_valid():
+		form.save()
+		return redirect('list-venues')
+
+	return render(request, 'events/update_venue.html', 
+		{'venue': venue, 'form':form}) 
+
+def search_venues(request):
+	if request.method == "POST":
+		searched = request.POST['searched']
+		venues = Venue.objects.filter(name__contains=searched)
+
+		return render(request, 'events/search_venues.html', {'searched':searched, 'venues':venues}) 
+	else:
+		return render(request, 'events/search_venues.html', {}) 
+
+
+def show_venue(request, venue_id):
+	venue = Venue.objects.get(pk=venue_id)
+	return render(request, 'events/show_venue.html', 
+		{'venue': venue}) 
+
+
+def list_venues(request):
+	venue_list = Venue.objects.all().order_by('name')
+
+	#set up pagination
+	p = Paginator(Venue.objects.all(), 1)
+	page = request.GET.get('page')
+	venues = p.get_page(page)
+	nums = "a" * venues.paginator.num_pages
+
+	return render(request, 'events/venue.html', 
+		{'venue_list': venue_list, 'venues': venues, 'nums':nums}) 
+
+def add_venue(request):
+	submitted = False
+	if request.method == "POST":
+		form = VenueForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect('/add_venue?submitted=True')
+	else:
+		form = VenueForm
+		if 'submitted' in request.GET:
+			submitted = True
+			
+	return render(request, 'events/add_venue.html', {'form':form, 'submitted':submitted}) 
+
+
+def all_events(request):
+	event_list = Event.objects.all().order_by('event_date')
+
+	#set up pagination
+	p = Paginator(Event.objects.all(), 2)
+	page = request.GET.get('page')
+	events = p.get_page(page)
+	nums = "a" * events.paginator.num_pages
+
+	return render(request, 'events/event_list.html', 
+		{'event_list': event_list, 'events': events, 'nums':nums}) 
+
+def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
+	name = "sarin"
+	month = month.capitalize()
+	#convert month from name to number
+	month_number = list(calendar.month_name).index(month)
+	month_number = int(month_number)
+
+	#create a calendar
+	cal = HTMLCalendar().formatmonth(year, month_number)
+
+	#get current year
+	now = datetime.now()
+	current_year = now.year
+
+	#get current time
+	time = now.strftime('%I:%M %p')
+
+	return render(request, 
+		'events/home.html', {
+		"name" : name,
+		"year" : year,
+		"month" : month,
+		"month_number" : month_number,
+		"cal" : cal,
+		"current_year" : current_year,
+		"time" : time,
+		})
---
+++ myclub_website/myclub_website/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('events.urls')),
+    path('members/', include('django.contrib.auth.urls')),
+    path('members/', include('members.urls')),
+
+]
+
+#configure admin titles
+admin.site.site_header = "My Club Administration"
+admin.site.site_title = "My Club"
+admin.site.index_title = "Admin"
